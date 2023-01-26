f5ea55673e766c34d4109239fef22c2931754bbe
Protogenic <vladimirbezrokov@gmail.com>
2023-01-26 15:59:14 +0300

    Second commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
 public class Main {
     public static void main(String[] args) {
+        // First contribution
         User user1 = new User("Alex", 30);
-        System.out.println("Hello " + user1);
+        System.out.println(user1);
+
+        // Added after first contribution
+        User user2 = new User("Bob", 22);
+        System.out.println(user2);
     }
 }
